4e360f174c97950a9839c7323d1e54950a323a83
Star CodeX <hello@bintangtobing.com>
2020-10-11 10:47:09 +0700

    update sunday

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+// Use Illuminate
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+// use Database
+
 
 class HomepageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('homepage.index');
+    }
 }
